1458871edec2ffd95f7424e3600f7cc4e54889cc
WonbeenLee-bskr <wb277@naver.com>
2023-06-23 14:35:50 +0900

    C언어 워크스페이스 생성

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,11 @@
+tr1 = 0; // 1byte
+    // int * ptr2 = 0; // 4byte
+    // double * ptr3 = 0; // 8byte
+
+    // printf("%d번지, %d번지, %d번지\n", ptr1, ptr2, ptr3);
+
+    // ptr1++;
+    // ptr2++;
+    // ptr3++;
+
+    // printf("%
